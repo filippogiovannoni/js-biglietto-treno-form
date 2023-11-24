fc9044b80ecad3245d9d08cfebeeba09ca3d913b
filippogiovannoni <filippogiovannonii@gmail.com>
2023-11-24 15:57:38 +0100

    added bottom section and name-price in page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,13 @@ Tariffario
 - > 65 anni: -40%
 */
 
-
-// Seleziono con il selettore il tag button e invoco la funzione anonima
-document.querySelector('button').addEventListener('click', function () {
+// Seleziono con il selettore la classe generate e invoco la funzione anonima
+document.querySelector('.generate').addEventListener('click', function () {
 
     // Al momento del click viene loggato in console il valore dei chilometri e dell'età
     console.log(km_percorsi.value);
     console.log(user_age.value);
+    document.getElementById('fullname').innerHTML = full_name.value
 
     // Se il passeggero ha un'età minore di 18 anni
     if (user_age.value < 18) {
@@ -28,8 +28,8 @@ document.querySelector('button').addEventListener('click', function () {
         const km_price = (0.21 * km_percorsi.value);
         const under_18 = (km_price - (km_price / 100 * 20));
 
-        //Stampo in console il prezzo scontato per i minorenni
-        console.log(under_18.toFixed(2) + "€");
+        //Stampo in pagina il prezzo scontato per i minorenni
+        document.getElementById('price').innerHTML = (under_18.toFixed(2) + "€");
     }
 
     // Altrimenti se il passeggero ha un'età superiore ai 65 anni
@@ -39,16 +39,16 @@ document.querySelector('button').addEventListener('click', function () {
         const km_price = (0.21 * km_percorsi.value);
         const over_65 = (km_price - (km_price / 100 * 40));
 
-        // Stampo in console il prezzo della tratta per il passeggero over 65
-        console.log(over_65.toFixed(2) + "€");
+        // Stampo in pagina il prezzo della tratta per il passeggero over 65
+        document.getElementById('price').innerHTML = (over_65.toFixed(2) + "€");
     }
 
     // // Altrimenti il passeggero non potrà usufruire di nessuna scontistica.
-     else {
+    else {
 
-        // // Stampo in console il prezzo della tratta per il passeggero.
+        // // Stampo in pagina il prezzo della tratta per il passeggero.
         const km_price = (0.21 * km_percorsi.value);
-        console.log(km_price.toFixed(2) + "€");
+        document.getElementById('price').innerHTML = (km_price.toFixed(2) + "€");
     }
 
 })
